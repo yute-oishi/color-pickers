d24a018c88b04f4d51885d8ed4dea57fe25906c1
Oishi.Yute <oishi.y@itix-system.co.jp>
2023-11-26 17:20:47 +0900

    update readme

--- src/modules/customDnd.ts
+++ src/modules/customDnd.ts
@@ -1,31 +1,7 @@
-function getItemGap(rects: ClientRect[], index: number, activeIndex: number) {
-  const currentRect = rects[index];
-  const previousRect = rects[index - 1];
-  const nextRect = rects[index + 1];
-
-  if (!currentRect || (!previousRect && !nextRect)) {
-    return 0;
-  }
-
-  if (activeIndex < index) {
-    return previousRect
-      ? currentRect.left - (previousRect.left + previousRect.width)
-      : nextRect.left - (currentRect.left + currentRect.width);
-  }
-
-  return nextRect
-    ? nextRect.left - (currentRect.left + currentRect.width)
-    : currentRect.left - (previousRect.left + previousRect.width);
-}
-
-const defaultScale = {
-  scaleX: 1,
-  scaleY: 1,
-};
-
 // eslint-disable-next-line react/jsx-props-no-spreading
 export const customStrategy = (_ref: any) => {
   var _rects$activeIndex;
+  const buttonHeight = 50;
 
   let {
     rects,
@@ -63,41 +39,54 @@ export const customStrategy = (_ref: any) => {
       ...defaultScale,
     };
   }
-  // 右、下方向に動くとき
+  // インデックスが大きい側（右、下）方向にドラッグしたとき
   if (index > activeIndex && index <= overIndex) {
+    // デフォルトのX移動量(ドラッグしているノードと同じ行内のノードのX移動量)
     let newX = -activeNodeRect.width - itemGap;
 
-    // 動作中ノードの2行以上↓の左端ノード専用(X)
+    // ドラッグ中ノードの1行以上↓に位置する左端ノード以外のX移動量
     if (
-      rects[index].bottom - rects[activeIndex].bottom > 100 &&
-      rects[index].bottom - rects[index - 1].bottom > 30
-    ) {
-      let gap = 1;
-      while (true) {
-        if (rects[index - gap].bottom - rects[index - gap - 1].bottom > 30) {
-          newX = -1 * rects[index - gap].width + rects[index - 1].right - 25;
-          break;
-        }
-        gap++;
-      }
-    }
-    // 動作中ノードの1行以上↓の左端ノード以外(X)
-    if (
-      rects[index].bottom - rects[activeIndex].bottom > 30 &&
+      rects[index].bottom - rects[activeIndex].bottom > buttonHeight &&
       rects[index].bottom == rects[index - 1].bottom
     ) {
       let gap = 0;
+      // 移動対象のノードの行の一番左端のノードを検出
       while (true) {
-        if (rects[index - gap].bottom - rects[index - gap - 1].bottom > 30) {
+        if (
+          rects[index - gap].bottom - rects[index - gap - 1].bottom >
+          buttonHeight
+        ) {
           newX = -1 * rects[index - gap].width;
           break;
         }
         gap++;
       }
     }
+
+    // ドラッグ中ノードの2行以上↓に位置する左端ノード専用のX移動量
+    if (
+      rects[index].bottom - rects[activeIndex].bottom > buttonHeight * 2 &&
+      rects[index].bottom - rects[index - 1].bottom > buttonHeight
+    ) {
+      let gap = 1;
+      // 移動対象のノードの行の一番左端のノードを検出
+      while (true) {
+        if (
+          rects[index - gap].bottom - rects[index - gap - 1].bottom >
+          buttonHeight
+        ) {
+          newX = -1 * rects[index - gap].width + rects[index - 1].right - 25;
+          break;
+        }
+        gap++;
+      }
+    }
+
+    // ドラッグ中ノードと同じ行にいるノード専用のY移動量 (デフォルト)
     let newY = 0;
-    // 動作中ノードの1行以上↓の全ノード(Y)
-    if (rects[index].bottom - rects[index - 1].bottom > 30) {
+
+    // 一番左端のノードの場合(移動対象ノードの前のノードが1行上にある場合)
+    if (rects[index].bottom - rects[index - 1].bottom > buttonHeight) {
       newY = -60;
     }
 
@@ -108,29 +97,18 @@ export const customStrategy = (_ref: any) => {
     };
   }
 
-  // 左、上方向に動くとき
+  // インデックスが小さい側（左、上）方向にドラッグしたとき
   if (index < activeIndex && index >= overIndex) {
+    // デフォルトのX移動量(ドラッグしているノードと同じ行内のノードのX移動量)
     let newX = activeNodeRect.width + itemGap;
-    // 動作中ノードの2行以上↑の右端ノード専用(X)
-    if (
-      rects[activeIndex].bottom - rects[index].bottom > 100 &&
-      rects[index + 1].bottom - rects[index].bottom > 30
-    ) {
-      let gap = 1;
-      while (true) {
-        if (rects[index + gap + 1].bottom - rects[index + gap].bottom > 30) {
-          newX = rects[index + gap].width - rects[index].right + 25;
-          break;
-        }
-        gap++;
-      }
-    }
-    // 動作中ノードの1行以上↑の右端ノード以外(X)
+
+    // ドラッグ中ノードの1行以上↑に位置する右端ノード以外のX移動量
     if (
       rects[activeIndex].bottom - rects[index].bottom > 30 &&
       rects[index].bottom == rects[index + 1].bottom
     ) {
       let gap = 0;
+      // 移動対象のノードの行の一番右端のノードを検出
       while (true) {
         if (rects[index + gap + 1].bottom - rects[index + gap].bottom > 30) {
           newX = rects[index + gap].width;
@@ -139,9 +117,30 @@ export const customStrategy = (_ref: any) => {
         gap++;
       }
     }
+
+    // ドラッグ中ノードの2行以上↑に位置する右端ノード専用のX移動量
+    if (
+      rects[activeIndex].bottom - rects[index].bottom > buttonHeight * 2 &&
+      rects[index + 1].bottom - rects[index].bottom > buttonHeight
+    ) {
+      let gap = 1;
+      // 移動対象のノードの行の一番右端のノードを検出
+      while (true) {
+        if (
+          rects[index + gap + 1].bottom - rects[index + gap].bottom >
+          buttonHeight
+        ) {
+          newX = rects[index + gap].width - rects[index].right + 25;
+          break;
+        }
+        gap++;
+      }
+    }
+
     let newY = 0;
-    // 動作中ノードの1行以上↑の全ノード(Y)
-    if (rects[index + 1].bottom - rects[index].bottom > 30) {
+
+    // 一番右端のノードの場合(移動対象ノードの次のノードが1行下にある場合)
+    if (rects[index + 1].bottom - rects[index].bottom > buttonHeight) {
       newY = 60;
     }
     return {
@@ -156,4 +155,31 @@ export const customStrategy = (_ref: any) => {
     y: 0,
     ...defaultScale,
   };
+};
+
+// 同じ行同士、ドラッグ中ノードと移動対象ノード間距離を取得する関数
+function getItemGap(rects: ClientRect[], index: number, activeIndex: number) {
+  const currentRect = rects[index];
+  const previousRect = rects[index - 1];
+  const nextRect = rects[index + 1];
+
+  if (!currentRect || (!previousRect && !nextRect)) {
+    return 0;
+  }
+
+  if (activeIndex < index) {
+    return previousRect
+      ? currentRect.left - (previousRect.left + previousRect.width)
+      : nextRect.left - (currentRect.left + currentRect.width);
+  }
+
+  return nextRect
+    ? nextRect.left - (currentRect.left + currentRect.width)
+    : currentRect.left - (previousRect.left + previousRect.width);
+}
+
+// スケール(ボタンの大きさ)拡大縮小はしない (1固定)
+const defaultScale = {
+  scaleX: 1,
+  scaleY: 1,
 };
